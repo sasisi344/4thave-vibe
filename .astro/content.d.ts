d2ccbf227522f961fe6fb27eb2e3629f5d59c5d3
sasisi344 <sasisi344@gmail.com>
2026-04-15 08:49:51 +0900

    next

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -122,12 +122,11 @@ declare module 'astro:content' {
 	type InferEntrySchema<C extends keyof DataEntryMap> = import('astro/zod').infer<
 		ReturnTypeOrOriginal<Required<ContentConfig['collections'][C]>['schema']>
 	>;
-	type ExtractLoaderConfig<T> = T extends { loader: infer L } ? L : never;
 	type InferLoaderSchema<
 		C extends keyof DataEntryMap,
-		L = ExtractLoaderConfig<ContentConfig['collections'][C]>,
+		L = Required<ContentConfig['collections'][C]>['loader'],
 	> = L extends { schema: import('astro/zod').ZodSchema }
-		? import('astro/zod').infer<L['schema']>
+		? import('astro/zod').infer<Required<ContentConfig['collections'][C]>['loader']['schema']>
 		: any;
 
 	type DataEntryMap = {
